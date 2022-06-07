0e67d5150cc7fce7d8944506f42ecd0de71d3aca
michael muliro <michael.muliro@student.moringaschool.com>
2022-06-07 19:22:26 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Code your solution in this file!
+const companyName = 'Scuber'; //defines the variable using the 'const' keyword ie not to be reassigned
+let mostProfitableNeighborhood = 'Chelsea' //defines using let, meaning it can be reassigned
+let companyCeo = 'Susan Smith'; //can also be reassigned 
